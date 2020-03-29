b088a3d79183a7b3b1a55d9e2e05c409ad90bfd3
BabaDaniel <56653978+BabaDaniel@users.noreply.github.com>
2020-03-29 21:42:28 +0200

    latest update

--- script.js
+++ script.js
@@ -40,3 +40,5 @@ TweenMax.from(".header", 0.8, {opacity: 0, x: 30, delay: 0.2});
 TweenMax.from(".subheader", 1, {opacity: 0, x: -30, delay: 0.6});
 TweenMax.from(".paragraph", 1, {opacity: 0, delay: 1, y: 20});
 TweenMax.from(".btn", 0.5, {y: 10, opacity: 0, delay: 1.6});  
+
+
